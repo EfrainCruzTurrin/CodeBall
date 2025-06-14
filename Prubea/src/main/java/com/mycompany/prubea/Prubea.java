d58ca8e221aca47a639247cf4aea1266ac41ddf0
EfrainCruzTurrin <efraincruzturrin@gmail.com>
2025-06-14 12:21:59 -0300

    gitignore
    Ignora archivos temp

---
+++ Prubea/src/main/java/com/mycompany/prubea/Prubea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prubea;
+
+/**
+ *
+ * @author EFRAIN
+ */
+public class Prubea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
