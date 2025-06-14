e159891ec45a63c74e1f710b22137a6ce77c877e
EfrainCruzTurrin <efraincruzturrin@gmail.com>
2025-06-14 11:18:27 -0300

    Primer commit CodeBall

---
+++ CodeBall/src/main/java/com/mycompany/codeball/CodeBall.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.codeball;
+
+/**
+ *
+ * @author EFRAIN
+ */
+public class CodeBall {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
