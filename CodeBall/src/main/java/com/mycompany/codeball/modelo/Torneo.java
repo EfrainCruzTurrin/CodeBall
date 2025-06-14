9b6542715541692dcde28eed686d03a132ecdba7
Ramiro-bit <ramirancogonzalez@gmail.com>
2025-06-14 12:59:16 -0300

    Modificacion de Equipo a lista.

--- CodeBall/src/main/java/com/mycompany/codeball/modelo/Torneo.java
+++ CodeBall/src/main/java/com/mycompany/codeball/modelo/Torneo.java
@@ -1,13 +1,14 @@
 package com.mycompany.codeball.modelo;
 
+import java.util.ArrayList;
+
 public class Torneo {
     private String descripcion;
-    private Equipo equipo;
+    private ArrayList<Equipo> equipos = new ArrayList<>();
     private Fase fase;
 
     public Torneo(String descripcion, Equipo equipo, Fase fase) {
         this.descripcion = descripcion;
-        this.equipo = equipo;
         this.fase = fase;
     }
 
@@ -18,10 +19,6 @@ public class Torneo {
         return descripcion;
     }
 
-    public Equipo getEquipo() {
-        return equipo;
-    }
-
     public Fase getFase() {
         return fase;
     }
@@ -30,17 +27,13 @@ public class Torneo {
         this.descripcion = descripcion;
     }
 
-    public void setEquipo(Equipo equipo) {
-        this.equipo = equipo;
-    }
-
     public void setFase(Fase fase) {
         this.fase = fase;
     }
 
     @Override
     public String toString() {
-        return "Torneo{" + "descripcion=" + descripcion + ", equipo=" + equipo + ", fase=" + fase + '}';
+        return "Torneo{" + "descripcion=" + descripcion + ", fase=" + fase + '}';
     }
     
     
