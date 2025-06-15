adab3136f652fa67940cbe219957dabe8e0b2167
JuanSFerratiCoser <juansebastiantec22@gmail.com>
2025-06-15 20:27:26 -0300

    Creación de la Clase Vista y detalles del Menú

--- CodeBall/src/main/java/com/mycompany/codeball/VistaTpi/Vista.java
+++ CodeBall/src/main/java/com/mycompany/codeball/VistaTpi/Vista.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.codeball.VistaTpi;
 
-/**
- *
- * @author EFRAIN
- */
+import java.util.Scanner;
+
+
 public class Vista {
+    private Scanner sc = new Scanner (System.in);  
     
+    public int pedirInt(){
+        return Integer.parseInt(sc.nextLine()); 
+    }
+    
+    public String pedirString(){
+        return (sc.nextLine()); 
+    }
+    
+    public void mensaje (String mensaje){
+        System.out.println(mensaje);
+    }
+    
+    public void mostrarMenu(){
+        System.out.println("Elija una opción: " +
+                "\n1-Registrar nuevo Jugador: " +
+                "\n2-Registrar nuevo Equipo: " +
+                "\n3-Registrar Árbitro: " +
+                "\n4-Registrar Torneo" +
+                "\n5-Lista de Jugadores: " +
+                "\n6-Lista de Equipos: " +
+                "\n0-Salir");
+    }
+
+
+    public int getOpcionMenu(){
+        this.mostrarMenu();                    // Mostrar las opciones
+        System.out.print("Ingrese la opción: ");
+        return this.sc.nextInt();              // Leer y retornar número elegido
+    }
 }
