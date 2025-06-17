50b87337c63926d7626c3a4135e3ea08a28d84e5
EfrainCruzTurrin <efraincruzturrin@gmail.com>
2025-06-17 09:33:47 -0300

    Conexion BD

---
+++ CodeBall/src/main/java/com/mycompany/codeball/conexion/ConectarBD.java
@@ -0,0 +1,23 @@
+package com.mycompany.codeball.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectarBD {
+
+    static String url = "jdbc:mysql://localhost:3306/codeball";
+    static String user = "root";
+    static String pass = "basededatos";
+
+    public static Connection conectar() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, user, pass);
+            System.out.println("Conexion exitosa");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
